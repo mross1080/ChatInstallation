25e0d4e94627d65a3353e680c7a06a75f974960c
Matt Ross <mattross1080@gmail.com>
2020-12-07 16:42:57 -0500

    adds opus

--- src/chatapp.js
+++ src/chatapp.js
@@ -86,7 +86,7 @@ const Testimony = ({ match }) => {
                 console.log("AUDIO PATH", message.msg_body.split(" ")[1])
                 let audioPath = message.msg_body.split(" ")[1]
                 audioPath = "/" + audioPath.replace(/[\u200B-\u200e\uFEFF]/g, '');
-                content = <audio controls><source src={audioPath} /></audio>
+                content = <audio controls><source src={audioPath} type="audio/ogg" codecs="opus"/></audio>
             }
             return <div className={(message.msg_sender == "admin" ? "msgRight " : "msgLeftContent ") + mediaTypeClass}>{content}</div>
 
